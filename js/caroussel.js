5817d25f8f28503dffb5d91827a3ca2db4b3d6c1
Matheus Viégas <matheusviegassouza4297@gmail.com>
2023-09-07 13:28:59 -0300

    Creat coletion the components

---
+++ js/caroussel.js
@@ -0,0 +1,11 @@
+//alert("Usamos kookies, ao clicar ok você está ciênte!");
+
+//Acessar a janela do brownser, o documento e selecionar o botão
+const btnRight = 
+window.document.querySelector('.button-arrow.-right')
+
+const elements = window.document.querySelector('')
+
+btnRight.addEventListener('click', function() {
+    console.log('Deu bom!')
+})
